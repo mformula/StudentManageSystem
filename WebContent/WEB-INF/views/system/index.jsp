2b8628c477e4ad4cefab769e8666f0f1faef3b59
mformula <mfomrula@126.com>
2020-05-29 17:10:04 +0800

    设置登录拦截器

--- WebContent/WEB-INF/views/system/index.jsp
+++ WebContent/WEB-INF/views/system/index.jsp
@@ -51,7 +51,7 @@
     <div region="north" split="true" border="false" style="overflow: hidden; height: 30px;
         background: url(images/layout-browser-hd-bg.gif) #7f99be repeat-x center 50%;
         line-height: 20px;color: #fff; font-family: Verdana, 微软雅黑,黑体">
-        <span style="float:right; padding-right:20px;" class="head"><span style="color:red; font-weight:bold;">${user.name}&nbsp;</span>您好&nbsp;&nbsp;&nbsp;<a href="SystemServlet?method=LoginOut" id="loginOut">安全退出</a></span>
+        <span style="float:right; padding-right:20px;" class="head"><span style="color:red; font-weight:bold;">${user.username}&nbsp;</span>您好&nbsp;&nbsp;&nbsp;<a href="SystemServlet?method=LoginOut" id="loginOut">安全退出</a></span>
         <span style="padding-left:10px; font-size: 16px; ">SWU学生信息管理系统</span>
     </div>
     <div region="south" split="true" style="height: 30px; background: #D2E0F2; ">
---
+++ src/org/mformula/interceptor/LoginInterceptor.java
@@ -0,0 +1,42 @@
+package org.mformula.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.mformula.entity.User;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+	@Override
+	public void afterCompletion(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, Exception arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void postHandle(HttpServletRequest arg0, HttpServletResponse arg1, Object arg2, ModelAndView arg3)
+			throws Exception {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object arg2) throws Exception {
+		
+		String url = request.getRequestURI();
+		User user = (User) request.getSession().getAttribute("user");
+		if(user==null) {
+			//表示未登录或者登录状态失效
+			System.out.println("未登录或者登录失效，url="+url);
+			//转到登录页面
+			response.sendRedirect(request.getContextPath()+"/system/login");
+			return false;
+		}
+		System.out.println(user.getUsername()+"登录成功，url="+url);
+		return true;
+	}
+
+}
